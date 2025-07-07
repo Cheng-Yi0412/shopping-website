a17e632c590b7f3c080ff2c0a4f638fb83d823ff
Cheng-Yi0412 <chengyiyu900412@gmail.com>
2025-07-08 01:50:13 +0800

    更改V1.1

--- functions.php
+++ functions.php
@@ -18,8 +18,11 @@ function logout(){          //登出:將session資料銷毀
 
 function checkLogin($email, $password){             //確認登入是否與資料庫符合
     global $dbconnection;
-    $staffQ=mysqli_query($dbconnection,"SELECT * FROM `staff` WHERE `email`='$email'");
-    $staff=mysqli_fetch_assoc($staffQ);
+    $staffQ=mysqli_prepare($dbconnection,"SELECT * FROM `staff` WHERE `email`=?");
+    mysqli_stmt_bind_param($staffQ, "s", $email);
+    mysqli_stmt_execute($staffQ);
+     $result = mysqli_stmt_get_result($staffQ);
+    $staff=mysqli_fetch_assoc($result);
 
     if($email === $staff['email'] && password_verify($password,$staff['password']) ){       //將輸入密碼和資料庫中hash過的亂碼比對
         $_SESSION['email']=$email;
